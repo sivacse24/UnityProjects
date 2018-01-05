ac89ba3de694a0a00971b03fde70990910a7a10c
Sivasankar <skandasmy@yume.com>
2018-01-05 20:49:18 +0530

    MPlayer
    Added MPlayer Setup

---
+++ GoogleMultiPlayer/Assets/Script/MovePlayer.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+using GooglePlayGames.BasicApi.Multiplayer;*/
+
+
+public class MovePlayer : MonoBehaviour {
+
+	[SerializeField]
+	private SliderBar hSlider,vSlider;
+	[SerializeField]
+	private float playerMoveSpeed=10;
+
+
+	[SerializeField]
+	private GameObject playerPrefeb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		if(playerPrefeb!=null)
+		{
+			playerPrefeb.transform.Translate (new Vector3(hSlider.sliderValue,0,vSlider.sliderValue)*Time.deltaTime*playerMoveSpeed);
+		}
+		else
+		{
+			//authenticateUser ();
+		}
+	}
+
+	/*private void authenticateUser()
+	{
+		PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+
+		PlayGamesPlatform.InitializeInstance(config);
+		// recommended for debugging:
+		PlayGamesPlatform.DebugLogEnabled = true;
+		// Activate the Google Play Games platform
+		PlayGamesPlatform.Activate();
+
+		PlayGamesPlatform.Instance.Authenticate((bool success) => {
+			// handle success or failure
+			if(success)
+			{
+				CreateQuickGame();
+			}
+			else
+			{
+				Debug.Log ("Failed to Login");
+			}
+		});
+	}
+
+	private void CreateQuickGame()
+	{
+		const int MinOpponents = 1, MaxOpponents = 2;
+		const int GameVariant = 0;
+		PlayGamesPlatform.Instance.RealTime.CreateQuickGame(MinOpponents, MaxOpponents,GameVariant, this);
+	}
+
+	#region RealTimeMultiplayerListener implementation
+
+	public void OnRoomSetupProgress (float percent)
+	{
+		//throw new System.NotImplementedException ();
+	}
+
+	public void OnRoomConnected (bool success)
+	{
+		//throw new System.NotImplementedException ();
+	}
+
+	public void OnLeftRoom ()
+	{
+		//throw new System.NotImplementedException ();
+	}
+
+	public void OnParticipantLeft (Participant participant)
+	{
+		//throw new System.NotImplementedException ();
+	}
+
+	public void OnPeersConnected (string[] participantIds)
+	{
+		//throw new System.NotImplementedException ();
+	}
+
+	public void OnPeersDisconnected (string[] participantIds)
+	{
+		//throw new System.NotImplementedException ();
+	}
+
+	public void OnRealTimeMessageReceived (bool isReliable, string senderId, byte[] data)
+	{
+		//throw new System.NotImplementedException ();
+	}
+
+	#endregion*/
+}
---
+++ GoogleMultiPlayer/Assets/Script/SliderBar.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SliderBar : MonoBehaviour,IPointerDownHandler,IPointerUpHandler {
+
+	private bool isMouseDown=false;
+	private Slider sliderBar;
+	public float sliderValue;
+
+	// Use this for initialization
+	void Start () {
+
+		sliderBar = GetComponent <Slider> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//print (isMouseDown);
+		if(!isMouseDown)
+		{
+			sliderBar.value = Mathf.Lerp (sliderBar.value, 0, Time.deltaTime*3.0f);//Vector3.Lerp ();
+		}
+		sliderValue = sliderBar.value;
+	}
+
+	#region IPointerDownHandler implementation
+
+	public void OnPointerDown (PointerEventData eventData)
+	{
+		isMouseDown = true;
+	}
+
+	#endregion
+
+	#region IPointerUpHandler implementation
+
+	public void OnPointerUp (PointerEventData eventData)
+	{
+		isMouseDown = false;
+	}
+
+	#endregion
+}
