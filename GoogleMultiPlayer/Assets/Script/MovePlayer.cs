b0fc39748fc23546b9e5d956ec6202f2d6462d40
Sivasankar <skandasmy@yume.com>
2018-01-05 23:48:21 +0530

    Added two player
    Yet to work on name players
    1.49.44

--- GoogleMultiPlayer/Assets/Script/MovePlayer.cs
+++ GoogleMultiPlayer/Assets/Script/MovePlayer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 using GooglePlayGames;
 using GooglePlayGames.BasicApi;
 using UnityEngine.SocialPlatforms;
@@ -17,13 +18,16 @@ public class MovePlayer : MonoBehaviour,RealTimeMultiplayerListener {
 
 
 	[SerializeField]
-	private GameObject playerPrefeb;
+	private Transform playerPrefeb;
+
+
+	private Transform myPlayer;
 
 	private bool isAuthenicatedOnce=false;
 	// Use this for initialization
 	void Start () 
 	{
-		
+		//myPlayer = Instantiate (playerPrefeb,Vector3.zero,Quaternion.identity);
 	}
 	
 	// Update is called once per frame
@@ -36,9 +40,12 @@ public class MovePlayer : MonoBehaviour,RealTimeMultiplayerListener {
 
 		}
 		
-		if(playerPrefeb!=null)
+		if(myPlayer!=null)
 		{
-			playerPrefeb.transform.Translate (new Vector3(hSlider.sliderValue,0,vSlider.sliderValue)*Time.deltaTime*playerMoveSpeed);
+			myPlayer.Translate (new Vector3(hSlider.sliderValue,0,vSlider.sliderValue)*Time.deltaTime*playerMoveSpeed);
+			string playerPos = myPlayer.position.x + ":"+myPlayer.position.y + ":"+myPlayer.position.z;
+			byte[] data = System.Text.ASCIIEncoding.Default.GetBytes (playerPos);
+			PlayGamesPlatform.Instance.RealTime.SendMessageToAll (false,data);
 		}
 	
 	}
@@ -72,9 +79,11 @@ public class MovePlayer : MonoBehaviour,RealTimeMultiplayerListener {
 
 	private void CreateQuickGame()
 	{
+		Debug.Log ("siva -- CreateQuickGame");
+
 		const int MinOpponents = 1, MaxOpponents = 2;
 		const int GameVariant = 0;
-		PlayGamesPlatform.Instance.RealTime.CreateQuickGame(MinOpponents, MaxOpponents,GameVariant, this);
+		PlayGamesPlatform.Instance.RealTime.CreateQuickGame(MinOpponents, MaxOpponents,GameVariant, this);//next go to OnRoomSetupProgress
 	}
 
 	#region RealTimeMultiplayerListener implementation
@@ -82,11 +91,34 @@ public class MovePlayer : MonoBehaviour,RealTimeMultiplayerListener {
 	public void OnRoomSetupProgress (float percent)
 	{
 		//throw new System.NotImplementedException ();
+		Debug.Log ("Siva -- OnRoomSetupProgress --"+ percent);
+		if(percent>=20)
+		{
+
+			Debug.Log ("Siva--Waiting for other players to join");
+			PlayGamesPlatform.Instance.RealTime.ShowWaitingRoomUI ();//OnRoomConnected
+		}
 	}
 
 	public void OnRoomConnected (bool success)
 	{
 		//throw new System.NotImplementedException ();
+		Debug.Log ("Siva--OnRoomConnected-"+success);
+
+
+		if(success)
+		{
+			myPlayer = Instantiate (playerPrefeb,Vector3.zero,Quaternion.identity);
+			myPlayer.name = PlayGamesPlatform.Instance.RealTime.GetSelf ().ParticipantId;
+			myPlayer.GetComponent <PlayerTextInfo>().updatePlayerName (PlayGamesPlatform.Instance.RealTime.GetSelf ().DisplayName);
+			byte[] data = new byte[1];
+			PlayGamesPlatform.Instance.RealTime.SendMessageToAll (true,data);
+
+		}
+		else
+		{
+			CreateQuickGame ();
+		}
 	}
 
 	public void OnLeftRoom ()
@@ -111,7 +143,33 @@ public class MovePlayer : MonoBehaviour,RealTimeMultiplayerListener {
 
 	public void OnRealTimeMessageReceived (bool isReliable, string senderId, byte[] data)
 	{
+		if (PlayGamesPlatform.Instance.RealTime.GetSelf ().ParticipantId.Equals (senderId))
+			return;
 		//throw new System.NotImplementedException ();
+		if(isReliable)
+		{
+			Transform tmpObj =Instantiate (playerPrefeb,Vector3.zero,Quaternion.identity);
+			tmpObj.name = senderId;
+		}
+		else
+		{
+			Transform targetObj = GameObject.Find (senderId).transform;
+
+			if (targetObj == null)
+				return;
+
+			//targetObj.GetComponent <PlayerTextInfo>().updatePlayerName (PlayGamesPlatform.Instance.RealTime.GetSelf ().DisplayName);
+
+			string postion = System.Text.ASCIIEncoding.Default.GetString (data);
+			string[] rawPos = postion.Split (new string[1]{ ":" }, System.StringSplitOptions.RemoveEmptyEntries);
+			Vector3 newPos = new Vector3 (
+				System.Convert.ToSingle (rawPos [0]), 
+				System.Convert.ToSingle (rawPos [1]), 
+				System.Convert.ToSingle (rawPos [2])
+			);
+
+			targetObj.position = Vector3.Lerp (targetObj.position,newPos,Time.deltaTime*3.0f);
+		}
 	}
 
 	#endregion
---
+++ GoogleMultiPlayer/Assets/Script/PlayerTextInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerTextInfo : MonoBehaviour {
+
+	// Use this for initialization
+	[SerializeField]
+	private Text nameTxt;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void updatePlayerName(string playerName)
+	{
+		nameTxt.text = playerName;
+	}
+}
